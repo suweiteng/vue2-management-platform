848c597a4276e7791263e064a3fd8d7b50eae2fe
suwt <891711995@qq.com>
2017-03-09 00:06:07 +0800

    修改配置文件

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, './dist'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
     productionSourceMap: true,
