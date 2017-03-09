f38db117f42b72b521817d237926af5dc1aeba1b
suwt <891711995@qq.com>
2017-03-09 09:27:13 +0800

    Initial dist subtree commit

--- README.md
+++
@@ -1,6 +0,0 @@
-# vue_ui_test
-vue2.0 &amp; elementUI 后台管理系统
-
-访问：https://reg21st.github.io/vue_ui_test/
-
-编写中 。已完成基本结构搭建
--- src/components/data/data.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>data</div>
-</template>
-<script type="text/ecmascript-6">
-export default {};
-</script>
-<style>
-</style>
