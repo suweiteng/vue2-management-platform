f133d6caf0e65316fc4c0c979c6b0e8cdae4c7ab
suwt <891711995@qq.com>
2017-03-17 20:41:11 +0800

    编辑方法完成

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -59,8 +59,8 @@
         </el-table>
       </el-col>
     </el-row>
-    <el-dialog title="修改个人信息" v-model="dialogFormVisible">
-      <el-form ref="form" :model="form" label-width="80px">
+    <el-dialog title="修改个人信息" v-model="dialogFormVisible" size="tiny">
+        <el-form ref="form" :model="form" label-width="80px">
         <el-form-item label="姓名">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
@@ -68,22 +68,10 @@
           <el-input v-model="form.address"></el-input>
         </el-form-item>
         <el-form-item label="出生日期">
-          <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="性别">
-          <el-radio-group v-model="form.resource">
-            <el-radio label="男"></el-radio>
-            <el-radio label="女"></el-radio>
-          </el-radio-group>
+            <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 100%;"></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">修改</el-button>
+          <el-button type="primary" @click="handleSave">修改</el-button>
           <el-button>取消</el-button>
         </el-form-item>
       </el-form>
@@ -111,13 +99,8 @@
         dialogFormVisible: false,
         form: {
           name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+          address: '',
+          date: '',
         }
       };
     },
@@ -144,8 +127,14 @@
         this.tableData.splice(index, 1);
       },
       handleEdit (index, row) {
-        this.dialogFormVisible = true
-      }
+        this.dialogFormVisible = true;
+        this.form = row
+      },
+      handleSave () {
+        this.$confirm('确认保存吗？', '提示', {}).then(() => {
+          this.dialogFormVisible = false;
+        });
+      },
     }
 };
 </script>
