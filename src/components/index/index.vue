4543c8be3e79d5dcad721bb9a8a4062e366ac206
suwt <891711995@qq.com>
2017-04-13 14:26:38 +0800

    增加首页组件 增加echart

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,8 +1,28 @@
 <template>
-    <div>待编辑</div>
+  <section class="chart-container">
+    <el-row>
+      <el-col :span="10" class="data_left data_left_li">
+        <div class="totale_home">共计  家</div>
+        <div id="chartPie" style="height:300px;">dddd</div>
+      </el-col>
+      <el-col :span="13" class="data_left data-left-line">
+        <div id="chartLine" style="height:300px;">dd</div>
+      </el-col>
+    </el-row>
+  </section>
 </template>
-<script type="text/ecmascript-6">
-export default {};
+<script>
+  import echarts from 'echarts'
+  export default {
+    data() {
+    },
+    methods: {
+      // 加载图表
+      getChartInit() {
+        console.log(echarts);
+      }
+    }
+  };
 </script>
 <style>
 </style>
