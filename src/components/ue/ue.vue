16cf17484c124d628289ccd59235d134aed10ea8
suweiteng <891711995@qq.com>
2017-07-11 21:36:46 +0800

    增加UE富文本编辑器

--- src/components/ue/ue.vue
+++ src/components/ue/ue.vue
@@ -21,13 +21,13 @@
     },
     mounted() {
       const _this = this;
-      this.editor = UE.getEditor('editor', this.config);
+      this.editor = UE.getEditor('editor', this.config); // 初始化UE
       this.editor.addListener("ready", function () {
-        _this.editor.setContent(_this.defaultMsg);
+        _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
       });
     },
     methods: {
-      getUEContent() {
+      getUEContent() { // 获取内容方法
         return this.editor.getContent()
       }
     },
